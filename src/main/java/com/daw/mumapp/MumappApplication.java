0d16bfd6e92c30d091b2ee338707e076ceb59b3b
ana <am.juradoc@alumnos.urjc.es>
2020-05-30 21:21:04 +0200

    Init Spring Boot project

---
+++ src/main/java/com/daw/mumapp/MumappApplication.java
@@ -0,0 +1,13 @@
+package com.daw.mumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MumappApplication.class, args);
+    }
+
+}
